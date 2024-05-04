a856b716d5be443f042fc0813b8014ca3d4ebea8
YutaKasuga <127809943+YutaKasuga@users.noreply.github.com>
2024-05-05 03:12:49 +0930

    Added codes to access initial screen

--- project/core/src/com/mygdx/game/Assignment2.java
+++ project/core/src/com/mygdx/game/Assignment2.java
@@ -1,31 +1,44 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-public class Assignment2 extends ApplicationAdapter {
+public class Assignment2 extends Game implements ApplicationListener {
 	SpriteBatch batch;
-	Texture img;
+	public static MenuScreen menuScreen;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		menuScreen = new MenuScreen(this, false);
+
+		// set the screen to menuScreen
+		setScreen(menuScreen);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		super.resize(width, height);
+	}
+
+	@Override
+	public void pause(){
+		super.pause();
+	}
+
+	@Override
+	public void resume(){
+		super.resume();
 	}
 }
